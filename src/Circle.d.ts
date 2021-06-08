e198e6bf4d88d9c3ac3ea77978941efa2f7de4be
Anubhav <iamanubhavsaini+git@gmail.com>
2021-06-08 14:54:10 +0530

    Refactor - Introduce interface for Circle

--- src/Circle.d.ts
+++ src/Circle.d.ts
@@ -3,7 +3,21 @@ interface DrawPatternI {
     strokePattern: StrokePatternT;
     lineWidth: number;
 }
-declare class Circle {
+interface CircleI {
+    startAngle: number;
+    endAngle: number;
+    antiClockwise: boolean;
+    context: CanvasRenderingContext2D;
+    radius: number;
+    draw(ofRadius: number, withPattern: DrawPatternI, atPosition: [number, number]): CircleI;
+    drawAt(x: number, y: number): CircleI;
+    drawWith(options: DrawPatternI): CircleI;
+    fill(ofRadius: number, withPattern: string, atPosition: [number, number]): CircleI;
+    fillAt(x: number, y: number): CircleI;
+    fillWith(fillPattern: StrokePatternT): CircleI;
+    of(radius: number): CircleI;
+}
+declare class Circle implements CircleI {
     startAngle: number;
     endAngle: number;
     antiClockwise: boolean;
